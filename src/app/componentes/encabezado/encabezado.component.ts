b55fd2db8c89df3e1e4387722899ff873b026e42
BeatrizG <beatrizgarciaang0@gmail.com>
2022-08-09 19:54:31 -0300

    Se agregan cambios en README.md

---
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+
+@Component({
+  selector: 'app-encabezado',
+  templateUrl: './encabezado.component.html',
+  styleUrls: ['./encabezado.component.css']
+})
+export class EncabezadoComponent implements OnInit {
+  miPortfolio:any;
+  barraList:any;
+  constructor(private datosPortfolio:PortfolioService) { }
+
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      this.miPortfolio=data;
+      this.barraList=data.barra;
+    });
+  }
+
+}
